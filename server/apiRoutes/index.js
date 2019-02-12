570265d5eef6bdb8248e138041858cc32bcead6b
FCraven <craven.francis@gmail.com>
2019-02-12 13:35:23 -0500

    404 error handler added to apiRoutes/index to return error if nothing is found

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -5,4 +5,12 @@ const router = require('express').Router();
 // router.use('/puppies', require('./puppies')); // matches all requests to  /api/puppies/
 // router.use('/kittens', require('./kittens')); // matches all requests to  /api/kittens/
 
+//404 Error Handler
+router.use(function(req,res,next){
+  const err = new Error('Notfound!');
+  err.status = 404;
+  next(err);
+})
+
+
 module.exports = router;
