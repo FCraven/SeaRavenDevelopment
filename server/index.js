3e71c946eb226562d2b094e163198dcc7828ea81
FCraven <craven.francis@gmail.com>
2019-02-12 13:17:17 -0500

    added express static serving middleware and hooked it up to the public folder

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 const morgan = require('morgan')
 const PORT = 3000;
 
-app.use(morgan(dev))
+//logging middleware
+app.use(morgan('dev'))
 
+//static serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
