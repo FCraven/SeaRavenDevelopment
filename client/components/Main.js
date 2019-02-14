4fd8c0542d5943470df7061fbd47f565daef57ae
FCraven <craven.francis@gmail.com>
2019-02-13 20:55:31 -0500

    end of day - notes in Main for next steps

--- client/components/Main.js
+++ client/components/Main.js
@@ -22,3 +22,9 @@ export default class Main extends Component {
 
 //use connect from react-redux to get slices of state
 // don't forget to map state and map dispatches to new componenets that need it
+
+//Add Header, Footer and some body components
+
+//Install Authentication stuff so you can log in and use the app
+
+// Think about DB schemas and what you'd like to add to theis thing
