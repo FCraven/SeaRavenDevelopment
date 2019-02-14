6a608c10ee9141b5991c7b04c6cf326d202dd915
FCraven <craven.francis@gmail.com>
2019-02-13 20:19:08 -0500

    imported style sheet into the client/index to be bundled by webpack

--- client/index.js
+++ client/index.js
@@ -1,8 +1,10 @@
+import '../public/styles.css'
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import Main from './components/Main'
+
 
 ReactDOM.render(
   <Provider store={store}>
