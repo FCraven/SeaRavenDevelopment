e683b2b7278759a341892b4c43101538cad6883f
FCraven <craven.francis@gmail.com>
2019-02-13 20:05:39 -0500

    barebones redux implementation: reducer created, store created, passed to Provider, Provider wraps Main component

---
+++ client/components/Main.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      <h1> Hello World. I exist.</h1>
+      </div>
+    )
+  }
+}
+
+
+//use connect from react-redux to get slices of state
+// don't forget to map state and map dispatches to new componenets that need it
--- client/index.js
+++ client/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import Main from './components/Main'
 
 ReactDOM.render(
-  <div>
-    <h1>Frank Craven Personal Site</h1>
-  </div>,
+  <Provider store={store}>
+    <Main />
+  </Provider>
+  ,
   document.getElementById('app')
 )
 
-//Start with boilermaker / redux
