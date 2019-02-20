4e687cc67da6c7c594440cfd1b18e7cb95a7b1fa
FCraven <craven.francis@gmail.com>
2019-02-20 16:32:52 -0500

    fleshing out navbar content to come soon

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -15,7 +15,9 @@ export default class Navbar extends Component {
       <header>SeaRaven Development</header>
       <h1> tomorrow begin crushing it</h1>
       <h1>Navbar</h1>
-      
+      <a>Certifications</a>
+      <a>Education</a>
+      <a>Experience</a>
       <a>Projects</a>
       <a>About Me</a>
       </div>
