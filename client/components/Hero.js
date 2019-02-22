6cec7941d6e9628ee19f7c647f4e1a2944ff9344
FCraven <craven.francis@gmail.com>
2019-02-21 19:23:55 -0500

    EOD

--- client/components/Hero.js
+++ client/components/Hero.js
@@ -3,17 +3,12 @@ import { Jumbotron } from 'react-bootstrap'
 
  const Hero =(props)=> {
   return (
-    <div>
-    <Jumbotron fluid>
-     {/* <Container> */}
-      <h1>Fluid jumbotron</h1>
-      <p>
-      This is a modified jumbotron that occupies the entire horizontal space of
-      its parent.
-      </p>
-    {/* </Container> */}
-    </Jumbotron>;
-  </div>
+    <div class='jumbotron jumbotron-fluid text-center hero-banner'>
+      <div class='container text-center'>
+        <h1>Having fun creating fun.</h1>
+        <p>This is a test case</p>
+      </div>
+    </div>
   )
 }
  export default Hero
