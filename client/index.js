d96f43a87a41101efa30694374e2362fd0bc7299
FCraven <craven.francis@gmail.com>
2019-03-02 18:40:18 -0500

    working on hero banner

--- client/index.js
+++ client/index.js
@@ -5,14 +5,12 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import Main from './components/Main'
-import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <Navbar />
     <Main />
     <Footer />
   </Provider>
